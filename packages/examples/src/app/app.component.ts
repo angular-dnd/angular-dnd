05b63cabccc034991d467e1db1b3809378bb12df
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-05-16 07:37:25 +0100

    added references to tsconfig.lib.json

--- packages/examples/src/app/app.component.ts
+++ packages/examples/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AngularDndService} from '@angular-dnd/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'examples';
+
+  constructor(
+    dnd: AngularDndService,
+  ) {
+  }
 }
