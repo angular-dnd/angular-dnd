e69a9ee4ec43e3504fd049b6cdd8befe4ef859c0
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-05-18 09:37:58 +0100

    updated deps to latest dnd-*

---
+++ publish.ps1
@@ -0,0 +1,5 @@
+Set-Location $PSScriptRoot
+npm publish dist/core --access public
+npm publish dist/sortable --access public
+npm publish dist/tree --access public
+npm publish dist/multi-backend --access public
