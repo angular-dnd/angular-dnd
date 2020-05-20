d74f912836c92c220a4dfb04bfa2204f2c528e50
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-05-20 12:25:59 +0100

    collapse sub pages menu items if top level is not selected

--- packages/examples/src/app/app.component.ts
+++ packages/examples/src/app/app.component.ts
@@ -6,55 +6,60 @@ import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {Router} from '@angular/router';
 
 interface IPage {
-  title: string;
-  url: string;
-  icon: string;
-  subPages?: IPage[];
+    title: string;
+    url: string;
+    icon: string;
+    subPages?: IPage[];
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  public selectedUrl = '/';
-  public appPages: IPage[] = [
-    {
-      title: 'Home',
-      url: '/',
-      icon: 'home',
-    },
-    {
-      title: 'Tree',
-      url: '/tree',
-      icon: 'git-merge',
-      subPages: [
+    public selectedUrl = '/';
+    public appPages: IPage[] = [
         {
-          title: 'Basic',
-          url: '/tree/basic',
-          icon: 'git-merge',
-        }
-      ]
-    },
-  ];
+            title: 'Home',
+            url: '/',
+            icon: 'home',
+        },
+        {
+            title: 'Tree',
+            url: '/tree',
+            icon: 'git-merge',
+            subPages: [
+                {
+                    title: 'Basic',
+                    url: '/tree/basic',
+                    icon: 'git-merge',
+                },
+                // {
+                //     title: 'Lines',
+                //     url: '/tree/lines',
+                //     icon: 'git-merge',
+                // },
+            ]
+        },
+    ];
 
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
-    router: Router,
-  ) {
-    this.initializeApp();
-    router.events.subscribe(() => {
-      this.selectedUrl = window.location.pathname.replace('/examples/', '/');
-    });
-  }
+    constructor(
+        private platform: Platform,
+        private splashScreen: SplashScreen,
+        private statusBar: StatusBar,
+        router: Router,
+    ) {
+        this.initializeApp();
+        router.events.subscribe(() => {
+            this.selectedUrl = window.location.pathname.replace('/examples/', '/');
+        });
+    }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            this.statusBar.styleDefault();
+            this.splashScreen.hide();
+        });
+    }
 }
