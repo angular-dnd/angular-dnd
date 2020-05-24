ab967cc93465eab85ec2856af778db8b653b7514
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-05-24 12:10:12 +0100

    tree: remove dependency on sortable package

--- packages/tree/src/lib/angular-dnd-tree.module.ts
+++ packages/tree/src/lib/angular-dnd-tree.module.ts
@@ -1,8 +1,6 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-// import {DndTreeItemDirective} from './directives/dnd-tree-item.directive';
 import {AngularDndCoreModule} from '@angular-dnd/core';
-import {AngularDndSortableModule} from '@angular-dnd/sortable';
 import {AngularDndTreeListComponent} from './components/angular-dnd-tree-list.component';
 import {AngularDndTreeItemComponent} from './components/angular-dnd-tree-item.component';
 import {AngularDndTreeComponent, DragPreviewTemplateDirective, TreeItemTemplateDirective} from './components/angular-dnd-tree.component';
@@ -15,18 +13,15 @@ const EXPORTS = [
   TreeItemTemplateDirective,
   DragPreviewTemplateDirective,
   AngularDndExternalTreeItemDirective,
-  // DndTreeItemDirective,
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     AngularDndCoreModule,
-    AngularDndSortableModule,
   ],
   declarations: [
     ...EXPORTS,
-    // DndTreeItemComponent,
   ],
   exports: EXPORTS,
 })
