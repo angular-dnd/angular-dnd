782e43cc3f13844db726e727c233e402070a22fb
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2020-07-04 21:41:07 +0100

    multi-backend/module.ts - no uage of const

--- packages/multi-backend/src/lib/module.ts
+++ packages/multi-backend/src/lib/module.ts
@@ -3,16 +3,16 @@ import {CommonModule} from '@angular/common';
 import {AngularDndPreviewComponent} from './preview.component';
 import {AngularDndPreviewRendererComponent} from './preview-renderer.component';
 
-/** @ignore */
-const EXPORTS = [
-  AngularDndPreviewComponent,
-  AngularDndPreviewRendererComponent,
-];
-
 @NgModule({
   imports: [CommonModule],
-  declarations: EXPORTS,
-  exports: EXPORTS,
+  declarations: [
+    AngularDndPreviewComponent,
+    AngularDndPreviewRendererComponent,
+  ],
+  exports: [
+    AngularDndPreviewComponent,
+    AngularDndPreviewRendererComponent,
+  ],
 })
 export class AngularDndMultiBackendModule {
 }
